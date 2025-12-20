172340071ed6c5db6d60f158980e854a4a227551
jakariaTalukdar <freelancerx2023@gmail.com>
2025-12-20 23:58:48 +0600

    header fixed

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,8 +3,17 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <nav className="bg-white text-gray-600 py-4 text-lg font-medium flex items-center justify-center gap-x-5">
-        <Link href="/">Men's Watch</Link>
+    <nav className="fixed top-0 bg-white/10 backdrop-blur-md shadow-md w-full text-gray-600 py-4 text-lg font-medium">
+        <div className='flex items-center justify-between gap-x-5 px-10'>
+            <div className='flex gap-x-4 text-xs md:text-sm'>
+                <Link href="/">Collections</Link>
+                <Link href="/">Watch Finder</Link>
+                <Link href="/">Learn about SEIKO</Link>
+                <Link href="/">Customer Service</Link>
+                <Link href="/">Stores</Link>
+
+            </div>
+        </div>
     </nav>
   )
 }
