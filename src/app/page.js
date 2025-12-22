e8a23b3e0426745ef518fc61b65632079b395537
jakariaTalukdar <freelancerx2023@gmail.com>
2025-12-22 22:10:39 +0600

    text section added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,19 @@ import React from 'react'
 
 export default function page() {
   return (
-    <HeroSlider/>
+    <div>
+      <HeroSlider/>
+      <div className='py-10 bg-black text-white'>
+        <div className='max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-between'>
+          <p>Important</p>
+          <div>
+            <p>Important Notice regarding Counterfeit and Modified SEIKO watches</p>
+            <p>Notice Concerning Succession of Clock Sales Business from Seiko Time Creation Inc.</p>
+            <p>[About the "fake accounts" on our official SNS]</p>
+          </div>
+          <a href="#">More</a>
+        </div>
+      </div>
+    </div>
   )
 }
