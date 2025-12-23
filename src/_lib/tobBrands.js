8192b831098e8bed49bac3128a98d2b8bed4fef5
jakariaTalukdar <freelancerx2023@gmail.com>
2025-12-23 21:08:00 +0600

    top brands demo component added

---
+++ src/_lib/tobBrands.js
@@ -0,0 +1,38 @@
+export const topBrands = [
+    {
+        name: "SEIKO",
+        image: "/images/top-brands/seiko.webp",
+    },
+    {
+        name: "Tissot",
+        image: "/images/top-brands/tissot.webp",
+    },
+    {
+        name: "CITIZEN",
+        image: "/images/top-brands/citizen.webp",
+    },
+    {
+        name: "Casio",
+        image: "/images/top-brands/casio.webp",
+    },
+    {
+        name: "Fossil",
+        image: "/images/top-brands/fossil.webp",
+    },
+    {
+        name: "G-Shock",
+        image: "/images/top-brands/g-shock.webp",
+    },
+    {
+        name: "Michel",
+        image: "/images/top-brands/michel.webp",
+    },
+    {
+        name: "Rado",
+        image: "/images/top-brands/rado.webp",
+    },
+    {
+        name: "Tommy Hilfiger",
+        image: "/images/top-brands/tommy.webp",
+    }
+];
