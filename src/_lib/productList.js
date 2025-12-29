b6e5c69103bfbd454efe30442e25ea0b00ef901d
jakariaTalukdar <freelancerx2023@gmail.com>
2025-12-29 21:47:53 +0600

    slug added with product item

--- src/_lib/productList.js
+++ src/_lib/productList.js
@@ -1,74 +1,80 @@
 export const productList = [
-    {
-      id: 1,
-      image: '/images/mockProduct/1.png',
-      image2: '/images/mockProduct/2.png',
-      title: 'Rolex Submariner',
-      brand: 'Rolex',
-      price: '৳1,250,000',
-      originalPrice: '৳1,400,000',
-      discount: '11% OFF',
-      rating: 4.8,
-      isNew: true
-    },
-    {
-      id: 2,
-      image: '/images/mockProduct/2.png',
-      image2: '/images/mockProduct/3.png',
-      title: 'Omega Speedmaster Moonwatch',
-      brand: 'Omega',
-      price: '৳850,000',
-      originalPrice: '৳920,000',
-      discount: '8% OFF',
-      rating: 4.7,
-      isNew: false
-    },
-    {
-      id: 3,
-      image: '/images/mockProduct/3.png',
-      image2: '/images/mockProduct/4.png',
-      title: 'TAG Heuer Carrera',
-      brand: 'TAG Heuer',
-      price: '৳650,000',
-      originalPrice: '৳720,000',
-      discount: '10% OFF',
-      rating: 4.5,
-      isNew: true
-    },
-    {
-      id: 4,
-      image: '/images/mockProduct/4.png',
-      image2: '/images/mockProduct/5.png',
-      title: 'Tissot PRX Powermatic 80',
-      brand: 'Tissot',
-      price: '৳120,000',
-      originalPrice: '৳135,000',
-      discount: '11% OFF',
-      rating: 4.6,
-      isNew: false
-    },
-    {
-      id: 5,
-      image: '/images/mockProduct/5.png',
-      image2: '/images/mockProduct/6.png',
-      title: 'Casio G-Shock GA-2100',
-      brand: 'Casio',
-      price: '৳25,000',
-      originalPrice: '৳28,000',
-      discount: '11% OFF',
-      rating: 4.9,
-      isNew: true
-    },
-    {
-      id: 6,
-      image: '/images/mockProduct/6.png',
-      image2: '/images/mockProduct/1.png',
-      title: 'Seiko Presage Cocktail Time',
-      brand: 'Seiko',
-      price: '৳85,000',
-      originalPrice: '৳95,000',
-      discount: '11% OFF',
-      rating: 4.7,
-      isNew: false
-    }
-  ];
+  {
+    id: 1,
+    image: '/images/mockProduct/1.png',
+    image2: '/images/mockProduct/2.png',
+    title: 'Rolex Submariner',
+    slug: 'rolex-submariner',
+    brand: 'Rolex',
+    price: '৳1,250,000',
+    originalPrice: '৳1,400,000',
+    discount: '11% OFF',
+    rating: 4.8,
+    isNew: true
+  },
+  {
+    id: 2,
+    image: '/images/mockProduct/2.png',
+    image2: '/images/mockProduct/3.png',
+    title: 'Omega Speedmaster Moonwatch',
+    slug: 'omega-speedmaster-moonwatch',
+    brand: 'Omega',
+    price: '৳850,000',
+    originalPrice: '৳920,000',
+    discount: '8% OFF',
+    rating: 4.7,
+    isNew: false
+  },
+  {
+    id: 3,
+    image: '/images/mockProduct/3.png',
+    image2: '/images/mockProduct/4.png',
+    title: 'TAG Heuer Carrera',
+    slug: 'tag-heuer-carrera',
+    brand: 'TAG Heuer',
+    price: '৳650,000',
+    originalPrice: '৳720,000',
+    discount: '10% OFF',
+    rating: 4.5,
+    isNew: true
+  },
+  {
+    id: 4,
+    image: '/images/mockProduct/4.png',
+    image2: '/images/mockProduct/5.png',
+    title: 'Tissot PRX Powermatic 80',
+    slug: 'tissot-prx-powermatic-80',
+    brand: 'Tissot',
+    price: '৳120,000',
+    originalPrice: '৳135,000',
+    discount: '11% OFF',
+    rating: 4.6,
+    isNew: false
+  },
+  {
+    id: 5,
+    image: '/images/mockProduct/5.png',
+    image2: '/images/mockProduct/6.png',
+    title: 'Casio G-Shock GA-2100',
+    slug: 'casio-g-shock-ga-2100',
+    brand: 'Casio',
+    price: '৳25,000',
+    originalPrice: '৳28,000',
+    discount: '11% OFF',
+    rating: 4.9,
+    isNew: true
+  },
+  {
+    id: 6,
+    image: '/images/mockProduct/6.png',
+    image2: '/images/mockProduct/1.png',
+    title: 'Seiko Presage Cocktail Time',
+    slug: 'seiko-presage-cocktail-time',
+    brand: 'Seiko',
+    price: '৳85,000',
+    originalPrice: '৳95,000',
+    discount: '11% OFF',
+    rating: 4.7,
+    isNew: false
+  }
+];
