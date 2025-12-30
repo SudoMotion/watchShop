21318ea4fd8e0d31676cbb69503f6f6099645184
jakariaTalukdar <freelancerx2023@gmail.com>
2025-12-31 00:11:29 +0600

    signup linked

--- src/component/Header.js
+++ src/component/Header.js
@@ -130,7 +130,7 @@ export default function Header() {
                     Sign In
                   </Link>
                   <Link 
-                    href="/register" 
+                    href="/signup" 
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     onClick={() => setShowUserMenu(false)}
                   >
