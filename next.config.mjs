1e8d5f08103211e2d69c26820467b0d69f19c7f8
fxjakaria04 <freelancerx2023@gmail.com>
2026-03-01 03:17:22 +0600

    third party image path added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {unoptimized: true}
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+    ],
+  },
 };
 
 export default nextConfig;
