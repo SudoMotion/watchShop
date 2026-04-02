00fae12da83ed807f0605c7fcdc64924c2b45bbb
fxjakaria04 <freelancerx2023@gmail.com>
2026-04-02 23:45:11 +0600

    order success

--- src/app/order/success/page.js
+++ src/app/order/success/page.js
@@ -31,7 +31,7 @@ function OrderSuccessContent() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12">
+    <div className="bg-gray-50 py-12">
       <div className="max-w-xl mx-auto px-4 text-center">
         <div className="bg-white rounded-xl shadow-sm p-8">
           <div className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center mx-auto mb-6">
