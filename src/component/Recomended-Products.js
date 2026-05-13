071a32cd57c296cb93d073856370e83af9c40760
fxjakaria04 <freelancerx2023@gmail.com>
2026-05-14 00:59:02 +0600

    recomended product section fixed

--- src/component/Recomended-Products.js
+++ src/component/Recomended-Products.js
@@ -1,16 +1,17 @@
-import { getDiscountProducts } from '@/stores/ProductAPI'
+import { getRecommendedProducts } from '@/stores/ProductAPI'
 import Link from 'next/link';
 import React from 'react'
 import BigButton from './BigButton';
 import ProductCard2 from './ProductCard2';
 
 export default async function RecomendedProducts({ sectionTitle = 'Recommended' }) {
-    const result = await getDiscountProducts();
-    const {products, brands, categories    } = result || {};
-    const items = (products?.data ?? (Array.isArray(products) ? products : [])).slice(0, 6);
+    // const result = await getDiscountProducts();
+    const {page_name, products} = await getRecommendedProducts()
+    // const {products, brands, categories    } = result || {};
+    const items = (products ?? (Array.isArray(products) ? products : [])).slice(0, 6);
   return (
     <div className='text-center p-5 pt-0'>
-        <h2 className='title leading-7'>{sectionTitle}</h2>
+        <h2 className='title leading-7'>{page_name}</h2>
         <div className='grid grid-cols-2 md:grid-cols-3 gap-x-5 gap-y-10'>
             {items.map((item, index) => (
             <ProductCard2 item={item} key={index} />
